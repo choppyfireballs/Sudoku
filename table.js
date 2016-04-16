93c11ff1526b297b644eec34a5277613bef0f4a4
david roszkowski <trumpetcrazy@gmail.com>
2016-04-16 11:15:40 -0500

    Added table.js for Eileen

---
+++ table.js
@@ -0,0 +1,11 @@
+//table
+for (var i=1; i < 10; i++) {
+    //open table row`
+    for (var j=0; j < 9; j++) {
+        //open table data
+        print j + i * 2;
+        //close table data 
+    }
+    //close table row
+}
+//close table
